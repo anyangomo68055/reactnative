d0628a659058ae816a170e6a4ae09adc3dc28162
anyangomo68055 <anyangomolouisanze@gmail.com>
2025-06-13 14:48:48 +0100

    first comit

---
+++ app/index1.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Index1 = () => {
+  return (
+    <View>
+      <Text>Index1</Text>
+    </View>
+  )
+}
+<h1>Hello how are you</h1>
+
+
+export default Index1
+
+const styles = StyleSheet.create({})
